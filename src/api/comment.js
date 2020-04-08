7eb059d61de69f69373ac4745336b4cfbee979f8
zyx <931890403@qq.com>
2020-04-09 07:56:57 +0800

    auth

---
+++ src/api/comment.js
@@ -0,0 +1,16 @@
+import R from './request.js';
+
+export default {
+    create: function() {
+
+    },
+    delete: function() {
+
+    },
+    update: function() {
+
+    },
+    getCommentsByPost: function() {
+
+    }
+}
